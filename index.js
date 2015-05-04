b3666216e9399a10158b6d781a3a8ed4cff9dfc8
MaksJS <maxence.derous@gmail.com>
2015-05-04 04:43:33 +0200

    added localStorage + other DOM

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 var jsdom = require('jsdom');
 var canvas = require('canvas');
-var document = jsdom.jsdom(null);
+var document = jsdom.jsdom("<html><body><canvas id='game'></canvas><input id='user-input' type='text' /></body></html>");
 
 global.document = document;
 global.Image = canvas.Image;
 global.window = document.parentWindow;
 global.PIXI = require('pixi.js/bin/pixi.js');
 global.p2 = require('p2');
+global.localStorage = require('localStorage');
 global.navigator = {userAgent: 'node.js'};
 global.window.Element = undefined;
-global.window.CanvasRenderingContext2D = 'undefined';
+global.window.CanvasRenderingContext2D = 'game';
 
 global.XMLHttpRequest = require('local-xmlhttprequest').XMLHttpRequest;
 
