af96e62488de7bc6348aa6115f35751104eaa65d
MaksJS <maxence.derous@gmail.com>
2015-07-14 14:24:30 +0200

    v0.0.5

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var jsdom = require('jsdom');
 var canvas = require('canvas');
-var document = jsdom.jsdom("<html><body><canvas id='game'></canvas><input id='user-input' type='text' /></body></html>");
+var document = jsdom.jsdom("<html><body></body></html>");
 
 global.document = document;
 global.Image = canvas.Image;
 global.window = document.parentWindow;
 global.PIXI = require('pixi.js/bin/pixi.js');
 global.p2 = require('p2');
-global.localStorage = require('localStorage'); // optional
 global.navigator = {userAgent: 'node.js'};
 global.window.Element = undefined;
 global.window.CanvasRenderingContext2D = 'game';
